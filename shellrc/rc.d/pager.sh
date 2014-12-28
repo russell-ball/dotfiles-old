a3453272f244944cdb2c89beaf3c60c4a376bd0a
Jacob Shafton <jacob@shafton.com>
2014-12-27 22:39:56 -0600

    Conditionally use vimpager instead of less

--- shellrc/rc.d/pager.sh
+++ shellrc/rc.d/pager.sh
@@ -1,2 +1,7 @@
-export MANPAGER="vimpager"
-export PAGER="vimpager"
+if which vimpager &> /dev/null; then
+  export MANPAGER="vimpager"
+  export PAGER="vimpager"
+else
+  export MANPAGER="less -X"
+  export PAGER="less"
+fi
