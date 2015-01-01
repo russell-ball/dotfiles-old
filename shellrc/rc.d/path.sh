a6fcf764096d43da5b72e0f516ed435809f18e77
Jacob Shafton <jacob@shafton.com>
2015-01-01 11:10:42 -0600

    GNU coreutils in the path if present

--- shellrc/rc.d/path.sh
+++ shellrc/rc.d/path.sh
@@ -1,4 +1,8 @@
 # Add `~/bin` to the `$PATH`
 export PATH="$HOME/bin:$PATH"
 
-# TODO: add gnu coreutils if necessary
+GNU_COREUTILS_PATH=/usr/local/opt/coreutils/libexec/gnubin
+[ -d $GNU_COREUTILS_PATH ] && PATH="$GNU_COREUTILS_PATH:$PATH"
+
+GNU_COREUTILS_MANPATH=/usr/local/opt/coreutils/libexec/gnuman
+[ -d $GNU_COREUTILS_MANPATH ] && MANPATH="$GNU_COREUTILS_MANPATH:$PATH"
