a2a2806702a0db4bb3662a7d83b7e08a4fd76fb3
Jacob Shafton <jacob@shafton.com>
2016-02-24 12:29:53 -0600

    fasd detection

--- shellrc/bashrc.d/fasd.bash
+++ shellrc/bashrc.d/fasd.bash
@@ -1,3 +1,5 @@
+if ! hash fasd 2> /dev/null; then exit 0; fi
+
 fasd_cache="$HOME/.fasd-init-bash"
 if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
   fasd --init posix-alias bash-hook bash-ccomp bash-ccomp-install >| "$fasd_cache"
