8f143b272f4f5ed3148dc46dcab08696612a2caf
Jacob Shafton <jacob@shafton.com>
2017-02-08 11:40:37 -0600

    Updated Homebrew bash completion

--- shellrc/bashrc.d/system_level_completions.bash
+++ shellrc/bashrc.d/system_level_completions.bash
@@ -1,9 +1,4 @@
 # If possible, add tab completion for many more commands
 [ -f /etc/bash_completion ] && source /etc/bash_completion
-
 # Potentially installed by Homebrew
-if [ -d /usr/local/etc/bash_completion.d ]; then
-  for file in /usr/local/etc/bash_completion.d/*; do
-    [ -e "$file" ] && source $file
-  done
-fi
+[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
