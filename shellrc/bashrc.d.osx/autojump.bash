10cea6262e3f6e79aa5d6b79b7b0a5e17ecbd7dc
Jacob Shafton <jacob@shafton.com>
2020-04-17 23:05:26 -0600

    Remove autojump in favor of fasd
    See fzf integration

--- shellrc/bashrc.d.osx/autojump.bash
+++
@@ -1,5 +0,0 @@
-# Configure auto-jump if installed via Homebrew
-hash brew &> /dev/null && \
-  AUTOJUMP_BREW_DIR=$(brew --prefix) && \
-  [[ -s "$AUTOJUMP_BREW_DIR/etc/autojump.sh" ]] && \
-  . "$AUTOJUMP_BREW_DIR/etc/autojump.sh"
