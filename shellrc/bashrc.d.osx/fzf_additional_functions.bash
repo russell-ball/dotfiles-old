1570421ebbea3561073da9b2057d07d2b204813b
Jacob Shafton <jacob@shafton.com>
2021-03-31 21:24:34 -0600

    Tweak format for fzf fshow function

--- shellrc/bashrc.d.osx/fzf_additional_functions.bash
+++ shellrc/bashrc.d.osx/fzf_additional_functions.bash
@@ -28,7 +28,7 @@ fcoc() {
 # fshow - git commit browser
 fshow() {
   git log --graph --color=always \
-      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+      --format="%C(auto)%h%d %an %s %C(black)%C(bold)%cr" "$@" |
   fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
       --bind "ctrl-m:execute:
                 (grep -o '[a-f0-9]\{7\}' | head -1 |
