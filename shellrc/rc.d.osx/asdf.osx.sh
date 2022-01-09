7d39d010a333030c822c057d907225417e76b67f
Jacob Shafton <jacob@shafton.com>
2022-01-09 16:29:23 -0700

    include bash completions for homebrew packages

--- shellrc/rc.d.osx/asdf.osx.sh
+++ shellrc/rc.d.osx/asdf.osx.sh
@@ -3,6 +3,3 @@
 if [ -f "${HOMEBREW_PREFIX}/opt/asdf/libexec/asdf.sh" ]; then
   . "${HOMEBREW_PREFIX}/opt/asdf/libexec/asdf.sh"
 fi
-if [ -f "${HOMEBREW_PREFIX}/opt/asdf/etc/bash_completion.d/asdf.bash" ]; then
-  . "${HOMEBREW_PREFIX}/opt/asdf/etc/bash_completion.d/asdf.bash"
-fi
--- shellrc/rc.d.osx/completions.osx.sh
+++ shellrc/rc.d.osx/completions.osx.sh
@@ -4,3 +4,16 @@ complete -W "NSGlobalDomain" defaults
 
 # Add `killall` tab completion for common apps
 complete -o "nospace" -W "Contacts Calendar Dock Finder Mail Safari iTunes SystemUIServer Terminal Twitter" killall
+
+# Add in all the completions installed with homebrew
+if type brew &>/dev/null
+then
+  if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
+    source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+  else
+    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*
+    do
+      [[ -r "${COMPLETION}" ]] && source "${COMPLETION}"
+    done
+  fi
+fi
