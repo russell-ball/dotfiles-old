daae5a7410cbc58f42f354a6acbcce1c32cb0d8d
Jacob Shafton <jacob@shafton.com>
2022-01-10 15:07:32 -0700

    finish conversion to asdf version manager

--- shellrc/rc.d.osx/nvm.osx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-command -v realpath_osx >/dev/null 2>&1 || realpath_osx() {
-  [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
-}
-
-export NVM_DIR=$(realpath_osx "$HOME/.nvm")
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
--- shellrc/rc.d.osx/pyenv.osx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
-fi
-
-if command -v pyenv-virtualenv-init >/dev/null; then
-  eval "$(pyenv virtualenv-init -)";
-fi
